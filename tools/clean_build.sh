259ffe27b281a7072ceb2da162cbb2e49dfb305e
Lara Maia <lara@craft.net.br>
2012-06-06 13:25:11 -0300

    Atualizando ferramentas

--- tools/clean_build.sh
+++ tools/clean_build.sh
@@ -3,4 +3,9 @@ cd ..
 rm -fv *.a
 rm -fv e4rat-lite-*
 rm -rfv build
+potd=(`find doc/ -type d -exec basename {} \;`)
+for ((i=1;i<${#potd[@]};i++)); do
+	 rm -fv doc/${potd[$i]}/*.8
+	 rm -fv doc/${potd[$i]}/*.5
+done
 exit 0
